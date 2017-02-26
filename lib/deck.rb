b5c9abcf6a01b0fd5d93a2abd06c2f625e0b70d4
Mirbek Askerov <mirbek.askerov@gmail.com>
2017-02-26 16:11:27 +0600

    Add class deck

---
+++ lib/deck.rb
@@ -0,0 +1,22 @@
+class Deck
+  attr_reader :cards
+
+  def initialize
+    @cards = Deck.build_cards
+  end
+
+  def self.build_cards
+    cards = []
+
+    [:валет, :дама, :король, :туз].each do |suit|
+      (2..10).each do |number|
+        cards << Card.new(suit, number)
+      end
+
+      ['J', 'Q', 'K', 'A'].each do |facecard|
+        cards << Card.new(suit, facecard)
+      end
+    end
+    cards.shuffle
+  end
+end
